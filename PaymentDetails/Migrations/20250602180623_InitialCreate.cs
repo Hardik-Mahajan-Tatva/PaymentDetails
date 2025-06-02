ad31edd9661695a8e7f89a951c1231defa8276b3
hrk62880 <hrk62880@gmail.com>
2025-06-02 23:56:35 +0530

    first commit

---
+++ PaymentDetails/Migrations/20250602180623_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PaymentDetails.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PaymentDetailsId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CardOwnerName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    CardNumber = table.Column<string>(type: "varchar(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "varchar(5)", nullable: false),
+                    SecurityCode = table.Column<string>(type: "varchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PaymentDetailsId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ PaymentDetails/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentDetails.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailsId{ get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string CardOwnerName { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(3)")]
+        public string SecurityCode { get; set; } = string.Empty;
+
+    }
+}
